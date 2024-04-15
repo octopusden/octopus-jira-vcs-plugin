8bbae1af8def81cf02f8b82c5bd5647d5699644e
Alexander Ryabokon <mr.alexander.ryabokon@gmail.com>
2024-04-15 16:47:45 +0300

    Add cache to IssueVcsSummary extraction

--- src/main/kotlin/org/octopusden/octopus/jira/vcs/config/PluginProperty.kt
+++ src/main/kotlin/org/octopusden/octopus/jira/vcs/config/PluginProperty.kt
@@ -2,9 +2,10 @@ package org.octopusden.octopus.jira.vcs.config
 
 enum class PluginProperty(val key: String, val displayName: String, val sensitive: Boolean) {
     VCS_FACADE_API_URL("vcs-facade.api.url", "VCS Facade API URL", false),
-    VCS_FACADE_RETRY_DELAY_MILLIS("vcs-facade.retry.delay.millis", "VCS Facade API retry timeout", false),
+    VCS_FACADE_RETRY_DELAY_MILLIS("vcs-facade.retry.delay.millis", "VCS Facade API retry timeout (msecs)", false),
 
-    VCS_PANEL_DISPLAY("octopus-vcs-plugin.vcs-panel.show", "Display VCS Panel", false);
+    VCS_PANEL_DISPLAY("octopus-vcs-plugin.vcs-panel.show", "Display VCS Panel", false),
+    VCS_PANEL_CACHE_SUMMARY_EXPIRE_AFTER_SECS("octopus-vcs-plugin.vcs-panel.cache.summary.expire-after.secs", "VCS Panel Summary Cache Expire After (secs)", false);
 
     companion object {
         fun valueOfPropertyName(propertyName: String): PluginProperty? {
--- src/main/kotlin/org/octopusden/octopus/jira/vcs/ui/vcspanel/VcsPanelContextProvider.kt
+++ src/main/kotlin/org/octopusden/octopus/jira/vcs/ui/vcspanel/VcsPanelContextProvider.kt
@@ -1,26 +1,48 @@
 package org.octopusden.octopus.jira.vcs.ui.vcspanel
 
+import com.atlassian.cache.Cache
+import com.atlassian.cache.CacheManager
+import com.atlassian.cache.CacheSettingsBuilder
 import com.atlassian.jira.issue.Issue
 import com.atlassian.jira.plugin.webfragment.contextproviders.AbstractJiraContextProvider
 import com.atlassian.jira.plugin.webfragment.model.JiraHelper
 import com.atlassian.jira.user.ApplicationUser
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport
 import com.atlassian.plugin.webresource.WebResourceUrlProvider
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import org.apache.velocity.tools.generic.DateTool
+import org.octopusden.octopus.jira.vcs.config.PluginProperty
+import org.octopusden.octopus.jira.vcs.config.PluginSettings
 import org.octopusden.octopus.jira.vcs.integration.vcsfacade.VcsFacadeService
 
 class VcsPanelContextProvider @Inject constructor(
     private val vcsFacadeService: VcsFacadeService,
-    @ComponentImport private val webResourceUrlProvider: WebResourceUrlProvider
+    @ComponentImport private val webResourceUrlProvider: WebResourceUrlProvider,
+    @ComponentImport private val cacheManager: CacheManager,
+    private val pluginSettings: PluginSettings
 ) : AbstractJiraContextProvider() {
+
+    private val cache: Cache<String, VcsFacadeService.IssueVcsSummary>? =
+        with(pluginSettings.getLong(PluginProperty.VCS_PANEL_CACHE_SUMMARY_EXPIRE_AFTER_SECS)) {
+            if (this > 0) {
+                cacheManager.getCache(
+                    "${VcsPanelContextProvider::class.qualifiedName}.cache",
+                    null,
+                    CacheSettingsBuilder().expireAfterWrite(
+                        this,
+                        TimeUnit.SECONDS
+                    ).build()
+                )
+            } else null
+        }
 
     override fun getContextMap(user: ApplicationUser, jiraHelper: JiraHelper): MutableMap<String, Any> {
         val contextMap = mutableMapOf<String, Any>()
         (jiraHelper.contextParams["issue"] as? Issue)
             ?.key
             ?.let { issueKey ->
-                val vcsInformation = vcsFacadeService.getSummary(issueKey)
+                val vcsInformation = cache?.get(issueKey) { issueKey.toVcsSummary() } ?: issueKey.toVcsSummary()
                 contextMap["branches"] = vcsInformation.branches
                 contextMap["commits"] = vcsInformation.commits
                 contextMap["pullRequests"] = vcsInformation.pullRequests
@@ -30,4 +52,6 @@ class VcsPanelContextProvider @Inject constructor(
             }
         return contextMap
     }
+
+    private fun String.toVcsSummary() = vcsFacadeService.getSummary(this)
 }
