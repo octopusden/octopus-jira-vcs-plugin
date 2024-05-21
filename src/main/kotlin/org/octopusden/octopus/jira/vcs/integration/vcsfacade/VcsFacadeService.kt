14a37a5d8fb3da675685ab2944ce649ed9d208f3
Alexander Ryabokon <mr.alexander.ryabokon@gmail.com>
2024-05-22 00:21:59 +0300

    Add file changes to commits view

--- src/main/kotlin/org/octopusden/octopus/jira/vcs/integration/vcsfacade/VcsFacadeService.kt
+++ src/main/kotlin/org/octopusden/octopus/jira/vcs/integration/vcsfacade/VcsFacadeService.kt
@@ -1,6 +1,7 @@
 package org.octopusden.octopus.jira.vcs.integration.vcsfacade
 
 import java.util.Date
+import org.octopusden.octopus.vcsfacade.client.common.dto.FileChangeType
 
 interface VcsFacadeService {
     fun updateConnection()
@@ -14,8 +15,10 @@ interface VcsFacadeService {
     )
 
     data class IssueBranchSummary(val size: Int, val updated: Date?)
-    data class IssuePullRequestSummary(val size: Int, val status: PullRequestStatus?, val updated: Date?) {
-        enum class PullRequestStatus(val style: String) {
+    data class IssuePullRequestSummary(val size: Int, val status: Status?, val updated: Date?) {
+
+        @Suppress("unused")
+        enum class Status(val style: String) {
             OPEN("info"),
             MERGED("success"),
             DECLINED("error")
@@ -23,7 +26,15 @@ interface VcsFacadeService {
     }
 
     data class IssueCommitSummary(val size: Int, val latest: Date?)
-    data class Commit(val sha: String, val url: String, val message: String, val date: Date, val author: Author)
+    data class Commit(
+        val sha: String,
+        val url: String,
+        val message: String,
+        val date: Date,
+        val author: Author,
+        val files: Collection<FileChange>
+    )
+
     data class Author(val avatar: String?, val name: String)
     data class RepositoryEntities<T : Any>(val url: String, val avatar: String?, val entities: Collection<T>) {
         val name: String
@@ -34,12 +45,31 @@ interface VcsFacadeService {
     data class Reviewer(val name: String, val avatar: String?, val approved: Boolean)
     data class Branch(val name: String, val url: String, val updated: Date)
 
+    data class FileChange(val type: Type, val url: String, val path: String) {
+        @Suppress("unused")
+        enum class Type(val style: String) {
+            ADD("success"),
+            MODIFY("new"),
+            DELETE("removed"),
+            UNCLASSIFIED("");
+
+            companion object {
+                fun valueOf(type: FileChangeType) = when (type) {
+                    FileChangeType.ADD -> VcsFacadeService.FileChange.Type.ADD
+                    FileChangeType.MODIFY -> VcsFacadeService.FileChange.Type.MODIFY
+                    FileChangeType.DELETE -> VcsFacadeService.FileChange.Type.DELETE
+                    else -> VcsFacadeService.FileChange.Type.UNCLASSIFIED
+                }
+            }
+        }
+    }
+
     data class PullRequest(
         val url: String,
         val title: String,
         val author: Author,
         val reviewers: Collection<Reviewer>,
-        val status: IssuePullRequestSummary.PullRequestStatus,
+        val status: IssuePullRequestSummary.Status,
         val updated: Date,
         val target: String
     )
--- src/main/kotlin/org/octopusden/octopus/jira/vcs/integration/vcsfacade/VcsFacadeServiceImpl.kt
+++ src/main/kotlin/org/octopusden/octopus/jira/vcs/integration/vcsfacade/VcsFacadeServiceImpl.kt
@@ -28,7 +28,7 @@ class VcsFacadeServiceImpl(
                     commits.size, commits.latest
                 ), VcsFacadeService.IssuePullRequestSummary(
                     pullRequests.size, pullRequests.status?.let { pullRequestStatus ->
-                        VcsFacadeService.IssuePullRequestSummary.PullRequestStatus.valueOf(
+                        VcsFacadeService.IssuePullRequestSummary.Status.valueOf(
                             pullRequestStatus.name
                         )
                     }, pullRequests.updated
@@ -37,15 +37,24 @@ class VcsFacadeServiceImpl(
         }
 
     override fun getCommits(issueKey: String): VcsFacadeService.Repositories<VcsFacadeService.Commit> =
-        with(vcsFacadeClient.findCommitsByIssueKey(issueKey.also { log.info("Get Commits for '{}'", it) })) {
-            val repositoryCommits = groupBy { c -> c.repository }
+        with(vcsFacadeClient.findCommitsWithFilesByIssueKey(issueKey.also {log.info("Get Commits for '{}'", it)})) {
+            val repositoryCommits = groupBy { c -> c.commit.repository }
                 .map { (repository, commits) ->
                     VcsFacadeService.RepositoryEntities(
                         repository.link,
                         repository.avatar,
                         commits.map { c ->
                             VcsFacadeService.Commit(
-                                c.id, c.link, c.message, c.date, VcsFacadeService.Author(c.author.avatar, c.author.name)
+                                c.commit.hash,
+                                c.commit.link,
+                                c.commit.message,
+                                c.commit.date,
+                                VcsFacadeService.Author(c.commit.author.avatar, c.commit.author.name),
+                                c.files.map { f ->
+                                    VcsFacadeService.FileChange(
+                                        VcsFacadeService.FileChange.Type.valueOf(f.type), f.link, f.path
+                                    )
+                                }
                             )
                         })
                 }
@@ -60,7 +69,7 @@ class VcsFacadeServiceImpl(
                     pr.title,
                     VcsFacadeService.Author(pr.author.avatar, pr.author.name),
                     pr.reviewers.map { r -> VcsFacadeService.Reviewer(r.user.name, r.user.avatar, r.approved) },
-                    VcsFacadeService.IssuePullRequestSummary.PullRequestStatus.valueOf(pr.status.name),
+                    VcsFacadeService.IssuePullRequestSummary.Status.valueOf(pr.status.name),
                     pr.updatedAt,
                     pr.target
                 )
@@ -79,7 +88,12 @@ class VcsFacadeServiceImpl(
         }
 
     private fun getClient(): VcsFacadeClient =
-        ClassicVcsFacadeClient(vcsFacadeClientParametersProvider).also { log.info("Init VCS Facade API client, URL: {}", vcsFacadeClientParametersProvider.getApiUrl()) }
+        ClassicVcsFacadeClient(vcsFacadeClientParametersProvider).also {
+            log.info(
+                "Init VCS Facade API client, URL: {}",
+                vcsFacadeClientParametersProvider.getApiUrl()
+            )
+        }
 
     companion object {
         private val log: Logger = LoggerFactory.getLogger(VcsFacadeServiceImpl::class.java)
